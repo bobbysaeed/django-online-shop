5852de99575e345ee90e45408d93f748ba61df2c
saeed <saeedbabakan10@gmail.com>
2024-11-04 21:53:38 -0800

    add product detail

--- products/urls.py
+++ products/urls.py
@@ -5,4 +5,6 @@ from .views import ProducListView, ProductDetailView
 
 urlpatterns = [
     path('', ProducListView.as_view(), name='product_list'),
+    path('<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+
 ]
