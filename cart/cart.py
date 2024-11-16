daec846eac0a17f0e1ad970c6f45dad11d21df55
saeed <saeedbabakan10@gmail.com>
2024-11-16 09:21:12 -0800

    create add to cart functionality

--- cart/cart.py
+++ cart/cart.py
@@ -1,7 +1,7 @@
 class Cart:
     def __init__(self, request):
-
-        """initialize the cart
+        """
+            initialize the cart
         """
         self.request = request
 
@@ -13,3 +13,22 @@ class Cart:
             self.session['cart'] = {}
             cart = self.session['cart']
         self.cart = cart
+
+    def add(self, product, quantity=1):
+        """
+            Add the specified product to the cart if it exists
+        """
+        product_id = str(product.id)
+
+        if product_id not in self.cart:
+            self.cart[product_id] = {'quantity': quantity}
+        else:
+            self.cart[product_id]['quantity'] += quantity
+
+        self.save()
+
+    def save(self):
+        """
+            Mark session as modified to save changes
+        """
+        self.session.modified = True
