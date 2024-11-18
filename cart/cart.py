02ac016f45d644d967e5b813bff7de6463b7a657
saeed <saeedbabakan10@gmail.com>
2024-11-18 06:58:40 -0800

    add functionality to count number of products of the cart

--- cart/cart.py
+++ cart/cart.py
@@ -48,9 +48,6 @@ class Cart:
             del self.cart[product_id]
             self.save()
 
-    def clear(self):
-        del self.session['cart']
-        self.save()
 
     def __iter__(self):
         product_ids = self.cart.keys()
@@ -63,3 +60,10 @@ class Cart:
 
         for item in cart.values():
             yield item
+
+    def __len__(self):
+        return sum(item['quantity'] for item in self.cart.values())
+
+    def clear(self):
+        del self.session['cart']
+        self.save()
