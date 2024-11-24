f26d7a37d22c6c59071a07ee6921139856e09844
saeed <saeedbabakan10@gmail.com>
2024-11-24 10:57:32 -0800

    add media

--- products/models.py
+++ products/models.py
@@ -8,6 +8,7 @@ class Product(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     price = models.PositiveIntegerField(default=0)
+    image = models.ImageField(verbose_name=_('Product Image'), upload_to='products/product_cover/', blank=True)
     active = models.BooleanField(default=True)
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
